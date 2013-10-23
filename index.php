29da6309804dfdb3d94069c0ebaaf06666153388
Seth <seth@eventespresso.com>
2013-10-23 15:13:01 -0600

    Adding PUE to the date range template. [touch: 3906]

--- index.php
+++ index.php
@@ -287,3 +287,34 @@ jQuery('tr.header').each(function(){
 
 <?php
 }
+
+/**
+ * hook into PUE updates
+ */
+//Update notifications
+add_action('action_hook_espresso_template_date_range_update_api', 'espresso_template_date_range_load_pue_update');
+function espresso_template_date_range_load_pue_update() {
+	global $org_options, $espresso_check_for_updates;
+	if ( $espresso_check_for_updates == false )
+		return;
+		
+	if (file_exists(EVENT_ESPRESSO_PLUGINFULLPATH . 'class/pue/pue-client.php')) { //include the file 
+		require(EVENT_ESPRESSO_PLUGINFULLPATH . 'class/pue/pue-client.php' );
+		$api_key = $org_options['site_license_key'];
+		$host_server_url = 'http://eventespresso.com';
+		$plugin_slug = array(
+			'premium' => array('p'=> 'espresso-template-date-range'),
+			'prerelease' => array('b'=> 'espresso-template-date-range-pr')
+			);
+		$options = array(
+			'apikey' => $api_key,
+			'lang_domain' => 'event_espresso',
+			'checkPeriod' => '24',
+			'option_key' => 'site_license_key',
+			'options_page_slug' => 'event_espresso',
+			'plugin_basename' => plugin_basename(__FILE__),
+			'use_wp_update' => FALSE
+		);
+		$check_for_updates = new PluginUpdateEngineChecker($host_server_url, $plugin_slug, $options); //initiate the class and start the plugin update engine!
+	}
+}
